f83b7fc49d00c6bc23047209ebb3f81172215143
anor23 <burgchiken@gmail.com>
2025-03-23 15:00:27 +0300

    модель продуктов зарегистрирована в админке

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from products.models import Product, Category, ParrentCategory
+
+
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk','name')
+    list_filter = ('price',)
+    ordering = ("pk",)
+
+
+
+@admin.register(Category)
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ('pk','name')
+
+
+@admin.register(ParrentCategory)
+class parentCategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk','name')
+    ordering = ('pk',)
