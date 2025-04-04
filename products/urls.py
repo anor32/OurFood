b3f128ffbf6da60811840d3d076327dbab46cda2
anor23 <burgchiken@gmail.com>
2025-04-04 20:57:02 +0300

    прописаны урлы к новым шаблонам

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,14 @@
+from itertools import product
+
 from django.urls import path
-from products.views import index
+from products.views import index_view, category_products_view, categories_list_view, all_list_products_view
 from products.apps import ProductsConfig
 
 app_name = ProductsConfig.name
 
 urlpatterns = [
-    path('',index,name='index'),
+    path('',index_view,name='index'),
+    path('Categories/',category_products_view, name = 'categories'),
+    path('categories/<int:pk>/products/',categories_list_view, name = 'categories_list'),
+    path('product/',all_list_products_view,name = 'products')
 ]
