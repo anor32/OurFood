977eb79204eb2d9da91baa6b203d006797e63690
anor23 <burgchiken@gmail.com>
2025-04-19 19:13:54 +0300

    модель юзера обновлена

--- users/models.py
+++ users/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
-NULLABLE = {"blank":True, "null": True}
+NULLABLE = {"blank": True, "null": True}
+
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin')
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
 
 
 class User(AbstractUser):
-    user_name = None
-    email = models.EmailField(unique= True, verbose_name="email")
-    phone = models.CharField(max_length=35,verbose_name="phone number", **NULLABLE)
-    telegram = models.CharField(max_length=150,verbose_name="telegram username", **NULLABLE)
-    date_birth = models.DateField(verbose_name="date birth")
-
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
+    email = models.EmailField(unique=True, verbose_name="Эл. почта")
+    phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE)
+    first_name = models.CharField(max_length=150, verbose_name="имя", default="Anonymous")
     is_active = models.BooleanField(default=True, verbose_name="activ")
+    address = models.CharField(max_length=150, verbose_name='адрес', **NULLABLE,)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f'{self.email}'
