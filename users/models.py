0d6144f31e431409749d42d4f4a4bfcc44fc6820
anor23 <burgchiken@gmail.com>
2025-04-20 19:55:09 +0300

    модель изменена добавлено имя пользователя изменено поле регистрации и входа красивый вид добавлен оптимизирован входи выход из аккаунта добавлена кнопка профиль и соответвующий шаблон

--- users/models.py
+++ users/models.py
@@ -12,11 +12,12 @@ class UserRoles(models.TextChoices):
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=150, verbose_name="имя", **NULLABLE)
     role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     email = models.EmailField(unique=True, verbose_name="Эл. почта")
     phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE)
-    first_name = models.CharField(max_length=150, verbose_name="имя", default="Anonymous" )
-    last_name = models.CharField(max_length=150, verbose_name="фамилия", default="Anonymous")
+    first_name = models.CharField(max_length=150, verbose_name="имя", default="Anonymous" ,**NULLABLE)
+    last_name = models.CharField(max_length=150, verbose_name="фамилия", default="Anonymous",**NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name="activ")
     address = models.CharField(max_length=150, verbose_name='адрес', **NULLABLE,)
 
