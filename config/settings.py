4b2a39409c8696b38a11a5117fd434254b148dad
anor23 <burgchiken@gmail.com>
2025-04-20 19:55:28 +0300

    определен loginredirecturl

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
+LOGIN_REDIRECT_URL = 'products:index'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
