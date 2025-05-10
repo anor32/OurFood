f9e3b297acb517f4fecaf509e3876dd4fe452c3f
anor23 <burgchiken@gmail.com>
2025-05-10 17:19:32 +0300

    обновлена модель добавлен атрибут для создания слайдера

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import ForeignKey
+from django.db.models import ForeignKey, BooleanField
 
 from users.models import NULLABLE
 
@@ -22,10 +22,12 @@ class ParrentCategory(models.Model):
         return self.name
 
 
-# а тут например родитель овощи и тут дальше идет огурцы помидоры разновидности сыры масла
+
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название подкатегории ")
     parent_category = ForeignKey(ParrentCategory, on_delete=models.CASCADE, verbose_name="Выбор Основной категории", **NULLABLE)
+    has_slider = models.BooleanField(default=False,verbose_name='Создать слайдер с категорией')
+
 
     class Meta:
         verbose_name = "Category"
@@ -55,13 +57,4 @@ class Product(models.Model):
         db_table = "goods"
         # get_latest_by ="Price"
 
-# честно сказать я ещё не особо решил что у меня будет за проект
-# наверноя я попробую делать доставку у меня большая проблема с идеями
-# единственное что мне приходит в голову это проект доставки так как я могу подсмотреть у других как лучше сделать
-# но это не точно
-# касательно остатков с базы данных я наверное просто свою базу напишу абстрактную а потом если будет надо реальный  сайт сделать
-# напишу код так чтобы просто другую базу данных подставить можно было и все
-# я надеюсь вы мне подскажете если что можно ли так вообще
-# в случае если подтянуть какую нибудь серьезную базу а там будут другие столбцы и не будут подходить к моей таблице
-# значит подгоню под свою таблицу
-# и возьму только те столбцы которые мне нужны
+
