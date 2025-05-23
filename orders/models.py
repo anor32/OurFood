4722cb38c36c25fa54a0f104480232f503af85a5
anor23 <burgchiken@gmail.com>
2025-05-23 18:31:12 +0300

    Cоздана модель заказа написана view для Создания таблицы с заказом

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,22 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
 
+from products.models import Product
+from users.models import NULLABLE
 
-
+from users.models import User
 # Create your models here.
 
 class Order(models.Model):
-    order_address =
+    order_user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, verbose_name="Заказчик")
+    products = models.ManyToManyField(Product, related_name="ordered_products")
+
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from orders.models import Order
+
 
 # Create your views here.
+
+
+
+class OrderCreate(CreateView):
+    model = Order
+
+    def post(self, request, *args, **kwargs):
+        Order.objects.create(
+            products = request.session['cart'],
+            order_user = request.user
+
+
+        )
+
+
+        return redirect(reverse('products:success_payment'))
+
