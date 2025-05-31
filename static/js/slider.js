f0c858e105f23f464b3f3c6a3260c03edcecc015
anor23 <burgchiken@gmail.com>
2025-05-31 19:23:50 +0300

    добавлен скрипт при котором страница не прокручивается наверх

--- static/js/slider.js
+++ static/js/slider.js
@@ -36,3 +36,15 @@ for (let i = 0; i < sliders.length; i++){
     });
 
 }
+
+window.addEventListener('beforeunload', function() {
+        sessionStorage.setItem('scrollPosition', window.scrollY);
+});
+
+
+window.addEventListener('load', function() {
+    const scrollPosition = sessionStorage.getItem('scrollPosition');
+    if (scrollPosition) {
+        window.scrollTo(0, parseInt(scrollPosition));
+    }
+})
