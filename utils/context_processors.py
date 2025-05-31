13373c42bc9dbeff2c70d83fe328bd8fc1f9e553
anor23 <burgchiken@gmail.com>
2025-05-31 17:49:40 +0300

    обновлен фронт корзины добавлен вывод общей суммы товаров в корзине

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -13,8 +13,12 @@ def category_context(request):
 
 
 def cart_context(request):
+        cart = request.session['cart']
         if "cart" in request.session:
-            return {"cart_products": request.session['cart']}
+            return {"cart_products": cart,
+                    "total_sum_products":sum(cart[index]['price'] for index in range(len(cart)))
+
+                    }
 
         return {}
 
